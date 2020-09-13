666becd0587723c05f983bd9b9b096dd65531c22
Wojciech Falkowski <wojciechxfalkowski@gmail.com>
2020-09-13 13:34:07 +0200

    Updates

---
+++ precache-manifest.2919163426edbcc33e70982d5cf7ffb0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26289504848ef12b62d17d59b2199a82",
+    "url": "/payment-app-react/index.html"
+  },
+  {
+    "revision": "43921b5ff64e8015f307",
+    "url": "/payment-app-react/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "43921b5ff64e8015f307",
+    "url": "/payment-app-react/static/js/2.c87c380e.chunk.js"
+  },
+  {
+    "revision": "254c01d1a3c7d8124adf4c35e46b76e3",
+    "url": "/payment-app-react/static/js/2.c87c380e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98a1ef3e50958718cf82",
+    "url": "/payment-app-react/static/js/main.ec54376f.chunk.js"
+  },
+  {
+    "revision": "18f75d0bd364a0043052",
+    "url": "/payment-app-react/static/js/runtime-main.9313ae33.js"
+  },
+  {
+    "revision": "f7babdbe97bc9488ccf6d37c79cec7b5",
+    "url": "/payment-app-react/static/media/plane-640×426.f7babdbe.jpg"
+  },
+  {
+    "revision": "7d792c29c02a99b2ed599dcdf5710e02",
+    "url": "/payment-app-react/static/media/shelby-640×350.7d792c29.jpg"
+  },
+  {
+    "revision": "2d180bd9f1c83e6c7f97d7f47f9e9eac",
+    "url": "/payment-app-react/static/media/ship-640x438.2d180bd9.jpg"
+  }
+]);
